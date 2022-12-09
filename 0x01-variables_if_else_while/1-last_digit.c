9a3f411150d73249f718c4862ec5fd504d6790e3
Its-Ajua <akinladerebecca04@gmail.com>
2022-12-09 09:17:05 -0800

    Using conditions

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ printf("Last digit of %d is %d and is 0\n", n, v);
 }
 else
 {
-printf("Last digit of %d is %d and is 0\n", n, v);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, v);
 }
 return (0);
 }
