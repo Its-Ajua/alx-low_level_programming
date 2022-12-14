9e183051bbcaf496c41434f04152953598d63872
Its-Ajua <akinladerebecca04@gmail.com>
2022-12-14 03:19:38 -0800

    prints the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,34 +7,27 @@
 void times_table(void)
 {
 	int i, j, k;
-
+	
+	for (i = 0; i <= 9; i++)
 	{
-		for (i = 0; i <= 9; i++)
+		_putchar('0');
+		for (j = 0; j <= 9; j++)
 		{
-			for (j = 0; j <= 9; j++)
+			k = i * j;
+			_putchar(',');
+			_putchar(' ');
+			
+			if (k <= 9)
 			{
-				k = j * i;
-				if (k == 0)
-				{
-					_putchar(k + '0');
-				}
-
-				if (k <= 9 && k != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				}
-				else if (k > 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				}
+				_putchar(' ');
+				_putchar(k + '0');
 			}
-			_putchar('\n');
+			else
+			{
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
 		}
+		_putchar('\n');
 	}
 }
