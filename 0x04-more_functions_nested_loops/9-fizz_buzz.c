8776044ff06947ac7ffb7ad19f296c7a4b3e497b
Its-Ajua <akinladerebecca04@gmail.com>
2022-12-15 13:46:16 -0800

    Fizz-Buss test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,9 @@
 #include "main.h"
 
 /**
- * main - FizzBuzz test
+ * main -  prints the numbers from 1 to 100, followed by a new line
+ * But for multiples of three print Fizz instead of the number
+ * and for the multiples of five print Buzz
  *
  * Return: 0
  */
