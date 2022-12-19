383577df8ca8b3eafe36b2c09fb2876c5e2cb4ac
Its-Ajua <akinladerebecca04@gmail.com>
2022-12-19 14:11:16 -0800

    Generating passwords

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -  program that generates random valid passwords 
+ *
+ * Return: 0.
+ */
+
+int main(void)
+{
+	printf("%d\n", rand());
+	printf("%d\n", rand());
+	printf("%d\n", rand());
+}
