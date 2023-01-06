3c6680a6af52ba5c0c8d455170bf6e38d5b03d4f
Its-Ajua <akinladerebecca04@gmail.com>
2023-01-06 08:41:52 -0800

    Addition

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function that adds integers
+ * @argc: counts argument
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, result = 0;
+
+	if (argc < 2)
+	{
+		printf("%d\n", 0);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (*argv[i] >= '0' && *argv[i] <= '9')
+			{
+				result += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", result);
+	}
+	return (0);
+}
