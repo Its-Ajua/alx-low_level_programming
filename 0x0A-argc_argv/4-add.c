49f6f6d7abd254300ab00102657cfd8fecb43900
Its-Ajua <akinladerebecca04@gmail.com>
2023-01-06 13:12:35 -0800

    Addition

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,7 +11,7 @@
 
 int main(int argc, char *argv[])
 {
-	int i, result = 0;
+	int i, j, result = 0;
 
 	if (argc < 2)
 	{
@@ -21,15 +21,15 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (*argv[i] >= '0' && *argv[i] <= '9')
+			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				result += atoi(argv[i]);
+				if (argv[i][j] < '0' || argv[i][j] > '9')
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
-			else if (*argv[i] <= '0' || *argv[i] >= '9')
-			{
-				printf("Error\n");
-				return (1);
-			}
+			result += atoi(argv[i]);
 		}
 		printf("%d\n", result);
 	}
