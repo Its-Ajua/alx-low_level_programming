b99c226c1f659f8ac1e82533edbf0a813000080b
Its-Ajua <akinladerebecca04@gmail.com>
2023-01-16 12:17:48 -0800

    Absolute Value

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,6 @@
-#ifndef ABSU
-#define ABSU
-#define ABS(x) (((x) < (0))? (-(x)) : (x))
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) (((x) < (0)) ? (-(x)) : (x))
+
 #endif
