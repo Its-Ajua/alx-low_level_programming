8e905d69865dcad919b78101c7643e522a2ff664
Its-Ajua <akinladerebecca04@gmail.com>
2023-01-31 17:00:07 -0800

    commits

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - function that prints a listint_t linked list
+ * @head: pointer to the first node of singly linked list
+ *
+ * Return: number of nodes in list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t x = 0;
+	const listint_t *previous;
+	const listint_t *current = head;
+
+	while (current != NULL)
+	{
+		printf("[%p] %d\n", (void *)current, current->n);
+		previous = current;
+		current = current->next;
+		x++;
+
+		if (previous <= current)
+		{
+			printf("-> [%p] %d\n", (void *)current, current->n);
+			break;
+		}
+	}
+	return (x);
+}
