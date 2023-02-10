4680e35aa1a9f39264d054aac8fb0d5570f486e2
Its-Ajua <akinladerebecca04@gmail.com>
2023-02-10 15:43:44 -0800

    Write

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,55 @@
+#include "main.h"
+/**
+ * _strlen - counts the length of a string
+ * @s: string
+ *
+ * Return: Length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+		return (0);
+	i++;
+	return (_strlen(s + 1));
+}
+
+/**
+ * create_file - creates files
+ * @filename: Name of file tp create
+ * @text_content: string to write the file
+ *
+ * Return: 1 on success or -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int new_file;
+	ssize_t write_file;
+
+	if (filename == NULL)
+		return (-1);
+
+	new_file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (new_file == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+		close(new_file);
+		return (1);
+	}
+
+	write_file = write(new_file, text_content, _strlen(text_content));
+
+	if (write_file == -1 || write_file != _strlen(text_content))
+	{
+		close(new_file);
+		return (-1);
+	}
+
+	close(new_file);
+	return (1);
+}
