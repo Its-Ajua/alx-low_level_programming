7153bb87467eb2263df429992cd117769921012c
root <akinladerebecca04@gmail.com>
2023-03-20 23:07:07 -0700

    LIb

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD="$PWD/ ../libgaga.so"
+wget -P /tmp https://github.com/Its-Ajua/alx-low_level_programming/raw/main/0x18-dynamic_libraries/milgaga.so
+export LD_PRELOAD=/tmp/milgaga.so
